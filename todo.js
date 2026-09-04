4f33d76ea0d017b9183673d74d882f8e89915f22
Ruskeer <107022660+Ruskeer@users.noreply.github.com>
2026-09-04 21:26:00 +0800

    Update todo.js

--- todo.js
+++ todo.js
@@ -16,12 +16,14 @@ function addTask() {
         span.innerHTML = '\u00d7'
         li.appendChild(span);
     }
+    taskInput.value = '';
+    saveData();
 }
 
 
 taskList.addEventListener("click", function(e) {
     if (e.target.tagName === "LI") {
-        e.target.classicList.toggle("checked");
+        e.target.classList.toggle("checked");
         saveData();
     }
     else if(e.target.tagName === "SPAN") {
